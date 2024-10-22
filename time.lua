393a7e39cb9926b1026e84441447d8f09af811bd
schindlershadow <schindlershadow@gmail.com>
2024-10-22 00:47:24 -0400

    add init support for pocket computers

--- time.lua
+++ time.lua
@@ -1,33 +1,39 @@
-monitor = peripheral.find("monitor")
-while monitor == nil do
+if pocket then
+    monitor = term
+else
     monitor = peripheral.find("monitor")
+    while monitor == nil do
+        monitor = peripheral.find("monitor")
+    end
+    monitor.setTextScale(1.9)
 end
+
 speaker = peripheral.find("speaker")
-monitor.setTextScale(1.9)
+
 
 function round(num, numDecimalPlaces)
-        local mult = 10^(numDecimalPlaces or 0)
-        return math.floor(num * mult + 0.5) / mult
+    local mult = 10 ^ (numDecimalPlaces or 0)
+    return math.floor(num * mult + 0.5) / mult
 end
 
 function playmusic(day)
     if day then
-        speaker.playNote("iron_xylophone",3,1)
+        speaker.playNote("iron_xylophone", 3, 1)
         sleep(0.5)
-        speaker.playNote("iron_xylophone",3,2)
+        speaker.playNote("iron_xylophone", 3, 2)
         sleep(0.5)
-        speaker.playNote("iron_xylophone",3,3)
+        speaker.playNote("iron_xylophone", 3, 3)
     else
-        speaker.playNote("iron_xylophone",3,3)
+        speaker.playNote("iron_xylophone", 3, 3)
         sleep(0.5)
-        speaker.playNote("iron_xylophone",3,2)
+        speaker.playNote("iron_xylophone", 3, 2)
         sleep(0.5)
-        speaker.playNote("iron_xylophone",3,1)
+        speaker.playNote("iron_xylophone", 3, 1)
     end
 end
 
 function monitorTick(formattedTime, cycle)
-    x,y = monitor.getSize()
+    x, y = monitor.getSize()
     if cycle == "sunrise" or cycle == "sunset" then
         local color = monitor.getBackgroundColour()
         if color == colors.blue then
@@ -41,19 +47,19 @@ function monitorTick(formattedTime, cycle)
         monitor.setBackgroundColour(colors.red)
     end
     monitor.clear()
-    monitor.setCursorPos(math.floor(1+(x/2)-(#formattedTime/2)),math.floor(y/2))
+    monitor.setCursorPos(math.floor(1 + (x / 2) - (#formattedTime / 2)), math.floor(y / 2))
     monitor.write(formattedTime)
     if cycle == "sunrise" then
-        monitor.setCursorPos(math.floor(1+(x/2)-(#"Sunrise!"/2)),1+math.floor(y/2))
+        monitor.setCursorPos(math.floor(1 + (x / 2) - (#"Sunrise!" / 2)), 1 + math.floor(y / 2))
         monitor.write("Sunrise!")
     elseif cycle == "sunset" then
-        monitor.setCursorPos(math.floor(1+(x/2)-(#"Sunset!"/2)),1+math.floor(y/2))
+        monitor.setCursorPos(math.floor(1 + (x / 2) - (#"Sunset!" / 2)), 1 + math.floor(y / 2))
         monitor.write("Sunset!")
     elseif cycle == "day" then
-        monitor.setCursorPos(math.floor(1+(x/2)-(#"Daytime"/2)),1+math.floor(y/2))
+        monitor.setCursorPos(math.floor(1 + (x / 2) - (#"Daytime" / 2)), 1 + math.floor(y / 2))
         monitor.write("Daytime")
     elseif cycle == "night" then
-        monitor.setCursorPos(math.floor(1+(x/2)-(#"Nighttime"/2)),1+math.floor(y/2))
+        monitor.setCursorPos(math.floor(1 + (x / 2) - (#"Nighttime" / 2)), 1 + math.floor(y / 2))
         monitor.write("Nighttime")
     end
 end
@@ -76,4 +82,4 @@ while true do
         monitorTick(formattedTime, "day")
     end
     sleep(0.5)
-end    
+end
